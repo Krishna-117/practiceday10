cbf784cf47f3258c14b55cbc075aaebace5da638
Krishna-J005 <krishna.jaiswal@timesinternet.in>
2021-07-19 15:57:05 +0530

    send request to server using json fake database

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,23 +1,26 @@
-import { useState } from "react"
+import axios from "axios";
+import {useState} from "react";
 
-
-export default function Userform() {
-    const [userform, setUserform] = useState({
-        firstname: "Ram",
-        age: 20
-    }); //hook -function
-    const handleEvent = function (event) {
+export default function UserForm(){
+    const[userform, setUserform]=useState({firstname:"Ram",age:"20"}); // hook function
+    
+    const handleEvent =function(event){
         console.log(event);
-        setUserform({ ...userform, [event.target.name]: event.target.value })
+        setUserform({...userform, [event.target.name]:event.target.value})
     }
-    const save = function (event) {
+    
+    const save = function(event){
         console.log(userform);
+        const promise= axios.post("http://localhost:4200/users", userform);
+        promise.then(function(response){
+            console.log(response);
+        })
     }
     return (
         <div>
             <h3>Create User</h3>
-            <input name="firstname" value={userform.firstname} onChange={handleEvent}></input>
-            <input name="age"value={userform.age} onChange={handleEvent}></input>
+            <input name='firstname' value={userform.firstname} onChange={handleEvent} />
+            <input  name='age' value={userform.age} onChange={handleEvent} />
             <button onClick={save}>Save</button>
         </div>
     )
