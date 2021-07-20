11ca36dfd51341225af6da09c2b0dd6401aa8021
Krishna-J005 <krishna.jaiswal@timesinternet.in>
2021-07-20 14:02:21 +0530

    Added skill dropbox and removed lint errors

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,33 +1,59 @@
 import { useState }
     from 'react'
 import axios from "axios"
+import Button from 'react-bootstrap/Button';
+import { Dropdown } from 'react-bootstrap';
+import { DropdownButton } from 'react-bootstrap';
 export default function UserForm() {
 
-    const [userForm, setUserform] = useState({ 
-        firstname: "Ram", 
-        age: "20",
-        joiningDate: "" }) //hook function
+    const [userForm, setUserform] = useState({
+        firstname: "",
+        age: "",
+        joiningDate: "",
+        skill: ""
+    }) //hook function
 
     const handleEvent = function (event) {
-        setUserform({ ...userForm,[event.target.name]: event.target.value });
+        setUserform({ ...userForm, [event.target.name]: event.target.value });
     }
-    const save = function (event) {
-      console.log("User first name: "+userForm.firstname);
-      console.log("User age: "+userForm.age);
-      const promise=axios.post("http://localhost:4200/users", userForm);
-      promise.then(function(response){
-          console.log(response);
-      })
+    const handleEvent2 = function (event) {
+        setUserform({ ...userForm, "skill": event });
+        
+    }
+        const save = function (event) {
+        console.log("User first name: " + userForm.firstname);
+        console.log("User age: " + userForm.age);
+        const promise = axios.post("http://localhost:4200/users", userForm);
+        promise.then(function (response) {
+            console.log(response);
+        })
 
     }
     return (
         <div>
             <h3>Create User form</h3>
-            <input placeholder='First Name' name='firstname'  value={userForm.firstname} onChange={handleEvent}>
-            </input>
-            <input placeholder='Age' name='age'value={userForm.age} onChange={handleEvent}></input>
-            joining date: <input name='joiningDate'type="date"value={userForm.joiningDate}  onChange={handleEvent}></input>
-            <button onClick={save}>Save</button>
+            <div className='form-group'>
+                <input placeholder='First Name' name='firstname' className='form-control' value={userForm.firstname} onChange={handleEvent}>
+                </input>
+            </div>
+            <input placeholder='Age' type='number' name='age' value={userForm.age} className='form-control' onChange={handleEvent}></input>
+            <label htmlFor='joiningDate'>Joining date:</label>
+            <div className='form-group'>
+                <input name='joiningDate' type="date" value={userForm.joiningDate} className='form-control' onChange={handleEvent}></input>
+            </div>
+            <br />
+            {<select name='skill' value={userForm.skill} onChange={handleEvent}>
+                <option>Select the Skill</option>
+                <option>HTML</option>
+                <option>JS</option>
+                <option>CSS</option>
+            </select>}
+            
+            <br />
+            <div className='form-group'>
+                <Button onClick={save} className='form-control'>Save</Button>
+            </div>
+
         </div>
     );
 }
--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 export default function Userlist() {
     const [users, setUsers] = useState([]);
     useEffect(() => {
-        if (users.length != 0) {
+        if (users.length !== 0) {
             return;
         }
         console.log('called......................');
@@ -15,13 +15,19 @@ export default function Userlist() {
         <div>
             <table className=' table table-bordered table-hover table-responsive table-striped'>
                 <thead>
-                    <tr><th>First name</th>
-                        <th>Age</th></tr>
+                    <tr>
+                        <th>First name</th>
+                        <th>Age</th>
+                        <th>Joining Date</th>
+                        <th>Skill</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    {users.map((user, index) => <tr>
+                    {users.map((user, index) => <tr key={index}>
                         <td>{user.firstname}</td>
                         <td>{user.age}</td>
+                        <td>{user.joiningDate}</td>
+                        <td>{user.skill}</td>
                     </tr>)}
                 </tbody>
             </table>
