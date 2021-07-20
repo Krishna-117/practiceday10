bcfa2d8b11989fa47b6484bfe3a01fe88dbb5251
Krishna-J005 <krishna.jaiswal@timesinternet.in>
2021-07-20 10:49:04 +0530

    display users in list

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,41 +1,44 @@
-import { useState } from "react"
 import axios from "axios";
 import { useEffect, useState } from "react"
 
-export default function Userlist() {
+export default function UserList() {
     const [users, setUsers] = useState([]);
     useEffect(function () {
-        if(users.length !=0){
+        if(users.length!=0){
             return;
         }
-        console.log('called......................');
+        console.log("called................")
         const promise = axios.get("http://localhost:4200/users");
         promise.then(function (response) {
-            setUsers(response.data);
-            console.log(response.data);
+            setUsers(response.data)
         })
     })
     return (
+
         <div>
-            <table className=' table table-bordered table-hover table-responsive'>
-            <table className=' table table-bordered table-hover table-responsive table-striped'>
-                <thead>
-                    <tr><td>First name</td>
-                    <td>Age</td></tr>
-                    <tr><th>First name</th>
-                        <th>Age</th></tr>
+            <table className="table table-bordered table-hover table-responsive">
+                <thead>git
+                    <tr>
+                        <th> FirstName</th>
+                        <th>Age</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Pariwesh</td>
-                        <td>34</td>
-                    </tr>
                     {users.map(function (user, index) {
-                        return <tr>
-                            <td>{user.firstname}</td>
-                            <td>{user.age}</td>
-                        </tr>
+                        return (
+                            <tr>
+                                <td>
+                                    {user.firstname}
+                                </td>
+                                <td>
+                                    {user.age}
+                                </td>
+                            </tr>
+                        )
                     })}
                 </tbody>
             </table>
+
         </div>
+    )
+}
