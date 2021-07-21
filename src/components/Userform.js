aab22a0a8eeb2ce5b1a40eca235ad7b2b8750cc9
Krishna-J005 <krishna.jaiswal@timesinternet.in>
2021-07-21 12:57:59 +0530

    added 2 way sorting

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -15,13 +15,13 @@ export default function Userform() { //stateful
 
     const save = function (event) {
         console.log(userform);
-        const promise = axios.post("http://localhost:4200/users", userform);
+        const promise = axios.post(process.env.REACT_APP_SERVER_URL, userform);
         promise.then(function (response) {
             console.log(response);
-            setMessage({...message, type: 'success', text: "Record was saved"})
+            setMessage({ ...message, type: 'success', text: "Record was saved" })
         });
         promise.catch(function (error) {
-            setMessage({...message, type: 'error', text: "Record was not saved" });
+            setMessage({ ...message, type: 'error', text: "Record was not saved" });
         })
     }
     const handleSelection = function (event) {
@@ -39,7 +39,7 @@ export default function Userform() { //stateful
             <div className="form-group">
                 <input type='date' className="form-control" name='joiningDate' value={userform.joiningDate} onChange={handleEvent}></input>
             </div>
-            <select value='default'  className='dropdown' name='skill' onChange={handleSelection} >
+            <select value='default' className='dropdown' name='skill' onChange={handleSelection} >
                 <option value='default'>Select the skill</option>
                 <option value='HTML'>HTML</option>
                 <option value='CSS'>CSS</option>
--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -4,17 +4,18 @@ import Counter from "./Counter";
 
 export default function Userlist() {
     const [users, setUsers] = useState([]);
+    const [flags, setFlags] = useState(true);
     useEffect(() => {
         // if (users.length !== -1) {
         //     return;
         // }
         console.log('called......................');
-        const promise = axios.get("http://localhost:4200/users");
+        const promise = axios.get(process.env.REACT_APP_SERVER_URL);
         promise.then(response => setUsers(response.data))
     }, [])
     const deleteUser = function () {
         // confirm("are you sure");
-        const promise = axios.delete("http://localhost:4200/users/" + this);
+        const promise = axios.delete(process.env.REACT_APP_SERVER_URL + this);
         promise.then(response => {
             //remove delete record from list
             users.splice(arguments[0], 1);
@@ -22,13 +23,20 @@ export default function Userlist() {
             setUsers(users1);
         })
     }
+    const sortByAge = () => {
+        setFlags(!flags)
+        users.sort((user1, user2) => flags ? user1.age - user2.age : user2.age - user1.age);
+        const users1 = [...users]; //time vs space complexity
+        setUsers(users1);
+    }
+
     return (
         <div>
-            { <Counter count={users.length}></Counter> }
+            {/* <Counter count={users.length}></Counter> */}
             <table className=' table table-compact table-bordered table-hover table-responsive table-striped'>
                 <thead>
                     <tr><th>First name</th>
-                        <th>Age</th>
+                        <th onClick={sortByAge.bind()}>Age</th>
                         <th>Joining Date</th>
                         <th>??</th></tr>
                 </thead>
